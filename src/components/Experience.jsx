37240098f7da6b84eee5c394ae325234d0a72fdb
Sergio Cabral <sergiocabral.1990@gmail.com>
2021-01-09 03:50:54 -0300

    First commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Experience = () => {
+
+    return (
+        <div>
+            Experience
+        </div>
+    )
+};
+
+export default Experience;
---
+++ src/components/References.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const References = () => {
+
+    return (
+        <div>
+            References
+        </div>
+    )
+};
+
+export default References;
